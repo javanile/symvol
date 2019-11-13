3f9a05117dfa4ca59d1aae59cafab9fbcd3b772e
Francesco Bianco <info.francescobianco@gmail.com>
2019-11-13 17:24:26 +0100

    starting testing env #3

--- tests/move-test.sh
+++ tests/move-test.sh
@@ -5,18 +5,26 @@ cd $(dirname "$0")/assert
 trap "echo $0: Test fail." ERR
 current=$(mktemp /tmp/symvol.test.XXXXXX)
 
-echo "---> Before testing"
-find . -name \* -print | sort - > ${current}
-diff assert.txt ${current}
+assert () {
+    find * -print | sort - > ${current}
+    diff ../$1.txt ${current}
+}
+
+echo "---> Before test"
+mkdir -p source target volume
+cp -rf ../fixtures/. source
+assert before
 
 echo "---> Assert #1"
-../symvol.sh move fixtures volume
-find . -name \* -print | sort - > ${current}
-diff move-test.txt ${current}
+../../symvol.sh move source target
+assert move-test-assert
 
 echo "---> Assert #2"
-../symvol.sh move volume fixtures
-find . -name \* -print | sort - > ${current}
-diff assert/move-test.txt ${current}
+../../symvol.sh move target source
+assert before
+
+echo "---> After test"
+rm -fr source/* target/* volume/*
+assert after
 
 echo "$0: Test success."
